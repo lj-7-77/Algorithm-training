b377fc3e5cac9ae9cafd69ea8d77280f0b335345
LJ <swtw72@gmail.com>
2023-05-07 16:33:48 +0900

    Update 달리기 경주.swift

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.swift
@@ -10,8 +10,23 @@ func solution(_ players:[String], _ callings:[String]) -> [String] {
         playersRace.swapAt(index-1, index-2)
         let frontPlayer: String = playersRace[index-1]
         
-        dict[callings[i]]! -= 1
-        dict[frontPlayer]! += 1
+        dict[callings[i]]! -= 1      //이름이 불린 선수의 등수 -1
+        dict[frontPlayer]! += 1     //앞에있던 선수의 등수 +1
     }
     return playersRace
 }
+
+/*
+ //오답 1: 시간초과(테스트7~13번)
+ // .firstIndex(of:) 의 시간복잡도 O(n)으로 인해 시간초과였던 것을 딕셔너리를 이용해 개선
+ func solution(_ players:[String], _ callings:[String]) -> [String] {
+     var playersRace = players
+     for name in 0..<callings.count {
+         let index = playersRace.firstIndex(of: callings[name])!
+         if index == 0 { continue }
+         playersRace.swapAt(index, index-1)
+     }
+     return playersRace
+ }
+ 
+ */
