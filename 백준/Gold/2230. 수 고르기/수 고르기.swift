ab2d59d940ef4cda6ff2fc0b944dc2f079c6517b
LJ <swtw72@gmail.com>
2023-05-25 08:39:37 +0900

    [Gold V] Title: 수 고르기, Time: 1440 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.swift
@@ -0,0 +1,26 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let n = input[0], m = input[1]
+var numbers = [Int]()
+
+func solution() {
+    var minValue = numbers.max()! - numbers.min()!
+    var end: Int = 0
+    
+    for start in 0..<n {
+        end = start
+        while end < n, numbers[end] - numbers[start] < m {
+            end += 1
+        }
+        if end == n { break }
+        minValue = min(minValue, numbers[end] - numbers[start])
+    }
+
+    print(minValue)
+}
+
+for _ in 0..<n {
+    numbers.append(Int(readLine()!)!)
+}
+
+numbers.sort()
+solution()
